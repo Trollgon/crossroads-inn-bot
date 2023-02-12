ad30b59930dfb9830d184ba173802ac5972aba9a
Taflaxx <13418652+Taflaxx@users.noreply.github.com>
2023-02-12 14:50:53 +0100

    compare gear with switched slots correctly

--- gw2/compare.py
+++ gw2/compare.py
@@ -1,40 +1,78 @@
+import copy
+
 from gw2.models.equipment import *
 from gw2.models.feedback import *
 
 
 def compare_weapons(player_equipment: Equipment, reference_equipment: Equipment) -> FeedbackGroup:
-    fbg = FeedbackGroup("Weapons")
+    fbg_1 = FeedbackGroup("Weapons")
+    fbg_2 = FeedbackGroup("Weapons")
 
-    for slot in ["WeaponA1", "WeaponA2", "WeaponB1", "WeaponB2"]:
-        if not compare_item(slot, player_equipment, reference_equipment, fbg, Rarity("Ascended")):
-            continue
+    # Create a copy of the player equipment with swapped weapons
+    player_equipment_tmp = copy.deepcopy(player_equipment)
+    tmp_1 = player_equipment_tmp.items["WeaponA1"] if "WeaponA1" in player_equipment_tmp.items else None
+    tmp_2 = player_equipment_tmp.items["WeaponA2"] if "WeaponA2" in player_equipment_tmp.items else None
+    player_equipment_tmp.items["WeaponA1"] = player_equipment_tmp.items["WeaponB1"] if "WeaponB1" in player_equipment_tmp.items else None
+    player_equipment_tmp.items["WeaponA2"] = player_equipment_tmp.items["WeaponB2"] if "WeaponB2" in player_equipment_tmp.items else None
+    player_equipment_tmp.items["WeaponB1"] = tmp_1
+    player_equipment_tmp.items["WeaponB2"] = tmp_2
 
-        # If items exist we can also check if upgrades match
-        reference_item: Item = reference_equipment.items[slot]
-        player_item: Item = player_equipment.items[slot]
+    # compare both player equipments with the reference equipment
+    for fbg, player_eq in [(fbg_1, player_equipment), (fbg_2, player_equipment_tmp)]:
+        using_correct_weapons = True
+        for slot in ["WeaponA1", "WeaponA2", "WeaponB1", "WeaponB2"]:
+            if not compare_item(slot, player_eq, reference_equipment, fbg, Rarity("Ascended")):
+                continue
 
-        if len(player_item.upgrades) < len(reference_item.upgrades):
-            fbg.add(Feedback(f"Your {player_item.type} is missing a sigil. "
-                             f"It needs a {' and a '.join(f'{upgrade}' for upgrade in reference_item.upgrades)}",
-                             FeedbackLevel.ERROR))
-            continue
+            # If items exist we can also check their type and if upgrades match
+            reference_item: Item = reference_equipment.items[slot]
+            player_item: Item = player_eq.items[slot]
 
-        sc_upgrades = reference_item.upgrades.copy()
-        player_upgrades = player_item.upgrades.copy()
-        for player_upgrade in player_upgrades:
-            for sc_upgrade in sc_upgrades:
-                if int(sc_upgrade.id) == int(player_upgrade.id):
-                    sc_upgrades.remove(sc_upgrade)
-                    player_upgrades.remove(player_upgrade)
-        if len(sc_upgrades) != 0 or len(player_upgrades) != 0:
-            fbg.add(Feedback(f"Your {player_item.type} has a {' and '.join(f'{upgrade}' for upgrade in player_upgrades)}"
-                             f" instead of a {' and '.join(f'{upgrade}' for upgrade in sc_upgrades)}",
-                             FeedbackLevel.WARNING))
+            # Check item type
+            if player_item.type != reference_item.type:
+                fbg.add(Feedback(f"Your {player_item.type} has to be a {reference_item.type}",
+                                 FeedbackLevel.ERROR))
+                using_correct_weapons = False
+                continue
 
-    if fbg.level <= FeedbackLevel.WARNING:
-        fbg.add(Feedback(f"All items are at least ascended", FeedbackLevel.SUCCESS))
-    if fbg.level <= FeedbackLevel.SUCCESS:
-        fbg.add(Feedback(f"Stats and upgrades of all items are correct", FeedbackLevel.SUCCESS))
+            # Check if upgrades exist
+            if len(player_item.upgrades) < len(reference_item.upgrades):
+                fbg.add(Feedback(f"Your {player_item.type} is missing a sigil. "
+                                 f"It needs a {' and a '.join(f'{upgrade}' for upgrade in reference_item.upgrades)}",
+                                 FeedbackLevel.ERROR))
+                continue
+
+            # Compare upgrades. Order of upgrades doesn't matter
+            sc_upgrades = reference_item.upgrades.copy()
+            player_upgrades = player_item.upgrades.copy()
+            for player_upgrade in player_upgrades:
+                for sc_upgrade in sc_upgrades:
+                    if int(sc_upgrade.id) == int(player_upgrade.id):
+                        sc_upgrades.remove(sc_upgrade)
+                        player_upgrades.remove(player_upgrade)
+            if len(sc_upgrades) != 0 or len(player_upgrades) != 0:
+                fbg.add(Feedback(f"Your {player_item.type} has a {' and '.join(f'{upgrade}' for upgrade in player_upgrades)}"
+                                 f" instead of a {' and '.join(f'{upgrade}' for upgrade in sc_upgrades)}",
+                                 FeedbackLevel.WARNING))
+
+        # Add positive feedback
+        if using_correct_weapons:
+            fbg.add(Feedback(f"You are using the correct weapons", FeedbackLevel.SUCCESS))
+        if fbg.level <= FeedbackLevel.WARNING:
+            fbg.add(Feedback(f"All items are at least ascended", FeedbackLevel.SUCCESS))
+        if fbg.level <= FeedbackLevel.SUCCESS:
+            fbg.add(Feedback(f"Stats and upgrades of all items are correct", FeedbackLevel.SUCCESS))
+
+    # Select the feedback with the least errors and return it
+    if fbg_1.level < fbg_2.level:
+        fbg = fbg_1
+    elif fbg_1.level > fbg_2.level:
+        fbg = fbg_2
+    else:
+        if len(fbg_1.feedback) <= len(fbg_2.feedback):
+            fbg = fbg_1
+        else:
+            fbg = fbg_2
     return fbg
 
 
@@ -61,8 +99,11 @@ def compare_armor(player_equipment: Equipment, reference_equipment: Equipment) -
 
 def compare_trinkets(player_equipment: Equipment, reference_equipment: Equipment) -> FeedbackGroup:
     fbg = FeedbackGroup("Trinkets")
-    for slot in ["Backpack", "Accessory1", "Accessory2", "Amulet", "Ring1", "Ring2"]:
+    for slot in ["Backpack", "Amulet"]:
         compare_item(slot, player_equipment, reference_equipment, fbg, Rarity("Ascended"))
+
+    for slots in [["Accessory1", "Accessory2"], ["Ring1", "Ring2"]]:
+        compare_item_group(slots[0], slots[1], player_equipment, reference_equipment, fbg, Rarity("Ascended"))
 
     if fbg.level <= FeedbackLevel.WARNING:
         fbg.add(Feedback(f"All items are at least ascended", FeedbackLevel.SUCCESS))
@@ -79,12 +120,12 @@ def compare_item(slot: str,
     if slot not in reference_equipment.items:
         return False
     reference_item: Item = reference_equipment.items[slot]
-    if slot not in player_equipment.items:
+    if slot not in player_equipment.items or player_equipment.items[slot] is None:
         fbg.add(Feedback(f"{reference_item.type} is missing", FeedbackLevel.ERROR))
         return False
     player_item: Item = player_equipment.items[slot]
 
-    # Compare Item stats
+    # Compare item stats
     if player_item.stats.name != reference_item.stats.name:
         fbg.add(Feedback(f"Your {player_item.stats.name} {player_item.type} should be {reference_item.stats.name}",
                          FeedbackLevel.WARNING))
@@ -93,3 +134,32 @@ def compare_item(slot: str,
     if player_item.rarity < min_rarity:
         fbg.add(Feedback(f"Your {player_item.type} has to be at least {min_rarity}", FeedbackLevel.ERROR))
     return True
+
+
+def compare_item_group(slot_1: str, slot_2: str,
+                       player_equipment: Equipment, reference_equipment: Equipment,
+                       fbg: FeedbackGroup = FeedbackGroup("Item comparison"),
+                       min_rarity: Rarity = Rarity("Exotic")) -> FeedbackGroup:
+    fbg_acc_1 = copy.deepcopy(fbg)
+
+    # Compare equipment to reference equipment
+    for slot in [slot_1, slot_2]:
+        compare_item(slot, player_equipment, reference_equipment, fbg_acc_1, min_rarity)
+
+    # Create a copy of the player equipment with the slots switched
+    player_equipment_tmp = copy.deepcopy(player_equipment)
+    tmp = player_equipment_tmp.items[slot_1]
+    player_equipment_tmp.items[slot_1] = player_equipment_tmp.items[slot_2]
+    player_equipment_tmp.items[slot_2] = tmp
+    fbg_acc_2 = copy.deepcopy(fbg)
+
+    # Compare equipment with switched slots to reference equipment
+    for slot in [slot_1, slot_2]:
+        compare_item(slot, player_equipment_tmp, reference_equipment, fbg_acc_2, min_rarity)
+
+    # Check which order has the least mistakes and return feedback from that one
+    if len(fbg_acc_1.feedback) <= len(fbg_acc_2.feedback):
+        fbg = fbg_acc_1
+    else:
+        fbg = fbg_acc_2
+    return fbg
