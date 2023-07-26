9feee0e11187de59e8c41ee61035c6b23dffb3c0
Taflaxx <13418652+Taflaxx@users.noreply.github.com>
2023-07-27 00:19:48 +0200

    added mech help command

--- src/cogs/mech_commands.py
+++ src/cogs/mech_commands.py
@@ -2,6 +2,7 @@ from discord import app_commands, Interaction, Embed
 from discord.ext import commands
 from sqlalchemy import select
 from database import Session
+from helpers.custom_embed import CustomEmbed
 from helpers.embeds import split_embed
 from models.boss import Boss
 from models.enums.mech_mode import MechMode
@@ -18,13 +19,38 @@ class MechCommands(commands.Cog):
     @app_commands.guild_only
     @app_commands.default_permissions(manage_roles=True)
     @app_commands.checks.has_permissions(manage_roles=True)
+    @mech.command(name="help", description="Show help page")
+    async def mech_help(self, interaction: Interaction):
+        embed = CustomEmbed(self.bot, title="Mechanics Help")
+        embed.description = "Mechanic checks can be used to instantly decline a log when a player/squad has failed it too many times." \
+                            "You can use the following subcommands to configure the mechanic checks."
+        embed.add_field(name="Subcommands",
+                        value="- `/mech list [encounter_id]`: Show a list of all configured mechanics\n"
+                              "- `/mech add: <encounter_id> <name> <max_amount> <mode>`: Add a mechanic check\n"
+                              "- `/mech delete <id>`: Delete a mechanic check\n"
+                              "- `/mech edit <id> [encounter_id] [name] [max_amount] [mode]`: Edit a mechanic check\n",
+                        inline=False)
+        embed.add_field(name="Arguments",
+                        value="- `encounter_id`: The encounter id of the boss. See `/boss list`\n"
+                              "- `name`: The name of the mechanic. See dps.report `Mechanics` tab\n"
+                              "- `max_amount`: The maximum amount of times the mechanic can be failed\n"
+                              "- `mode`: The mode of the mechanic check:\n"
+                              " - `PLAYER`: Only check for the player that submitted the log\n"
+                              " - `SQUAD`: Check the sum of failures of all players in the squad\n",
+                        inline=False)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+
+
+    @app_commands.guild_only
+    @app_commands.default_permissions(manage_roles=True)
+    @app_commands.checks.has_permissions(manage_roles=True)
     @mech.command(name="list", description="List all mechanic checks.")
     async def mech_list(self, interaction: Interaction, encounter_id: int = None):
         stmt = select(Mech).order_by(Mech.encounter_id, Mech.id)
         if encounter_id:
             stmt = stmt.where(Mech.encounter_id == encounter_id)
 
-        embed = Embed(title="Mechanics", description="[id] name: max_amount (mode)")
+        embed = CustomEmbed(self.bot ,title="Mechanics", description="[id] name: max_amount (mode)")
         async with Session.begin() as session:
             mechs = (await session.execute(stmt)).scalars()
             encounter_id = -1
